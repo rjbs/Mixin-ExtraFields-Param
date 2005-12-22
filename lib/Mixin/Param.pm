b924879b484a32c7b46106b492d033ed69f7a979
Ricardo SIGNES <rjbs@codesimply.com>
2005-12-22 21:29:07 +0000

    one more name change thing needed

--- lib/Mixin/Param.pm
+++ lib/Mixin/Param.pm
@@ -105,7 +105,7 @@ Ricardo Signes, C<< <rjbs@cpan.org> >>
 =head1 BUGS
 
 Please report any bugs or feature requests to
-C<bug-does-param@rt.cpan.org>, or through the web interface at
+C<bug-mixin-param@rt.cpan.org>, or through the web interface at
 L<http://rt.cpan.org>.  I will be notified, and then you'll automatically be
 notified of progress on your bug as I make changes.
 
