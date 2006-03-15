437e306c5e4ea00d0751ec4cc6b26eb8755b23f2
Ricardo SIGNES <rjbs@codesimply.com>
2006-03-15 01:16:29 +0000

    r19787@knave:  rjbs | 2006-03-14 18:22:16 -0500  better docs, add delete_param

--- lib/Mixin/Param.pm
+++ lib/Mixin/Param.pm
@@ -5,8 +5,12 @@ use strict;
 
 use Carp ();
 use Scalar::Util ();
-use Sub::Install ();
 use Tie::RefHash::Weak;
+
+use Sub::Exporter -setup => {
+  exports  => [ qw(param) ],
+  groups   => { default => [ qw(param) ] },
+};
 
 =head1 NAME
 
@@ -52,7 +56,8 @@ provided by L<CGI>, L<CGI::Application>, and other classes.
 
 =cut
 
-tie my %_params_for, 'Tie::RefHash::Weak';
+my %_params_for;
+BEGIN { tie %_params_for, 'Tie::RefHash::Weak'; }
 
 sub __params_storage_guts { %_params_for }
 
@@ -76,30 +81,49 @@ sub param {
     return $stash->{$key};
   }
 
-  my @return;
+  my @assigned;
   while (@_) {
+    # We don't put @_ into a hash because we guarantee processing (and more
+    # importantly return) order. -- rjbs, 2006-03-14
     my ($key, $value) = splice @_, 0, 2;
     $stash->{$key} = $value;
-    push @return, $value;
+    push @assigned, $value;
   }
-  return wantarray ? @return : $return[0];
+  return wantarray ? @assigned : $assigned[0];
 }
 
-sub import {
-  my ($class, $import_as) = @_;
-  my $into = caller(0);
-  $import_as ||= 'param';
+=head2 C< delete_param >
 
-  Sub::Install::install_sub({
-    code => \&param,
-    into => $into, 
-    as   => $import_as
-  });
+ my $deleted = $object->delete_param('name'); # delete the param entirely
+
+ my @deleted = $object->delete_param(@names); # delete several params
+
+ my $deleted = $object->delete_param(@names); # returns the first deleted value
+
+=cut
+
+sub delete_param {
+  my $self = shift;
+  my (@keys) = @_;
+  
+  Carp::croak "delete_param is an instance method"
+    unless Scalar::Util::blessed($self);
+
+  my $stash = $_params_for{ $self } ||= {};
+
+  my @deleted = map { scalar delete $stash->{$_} } @keys;
+
+  return wantarray ? @deleted : $deleted[0];
 }
 
 =head1 AUTHOR
 
 Ricardo Signes, C<< <rjbs@cpan.org> >>
+
+=head2 THANKS
+
+Thanks to Yuval Kogman, not only for writing Tie::WeakRefHash, but for pointing
+it out to me when I said that someone should write it.
 
 =head1 BUGS
 
@@ -110,7 +134,7 @@ notified of progress on your bug as I make changes.
 
 =head1 COPYRIGHT
 
-Copyright 2005 Ricardo Signes, All Rights Reserved.
+Copyright 2005-2006 Ricardo Signes, All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
